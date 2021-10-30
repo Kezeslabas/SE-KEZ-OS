9860f5eb3a3516eeee58c624db2d82416205cc6a
Kezeslabas <cari.gergo@gmail.com>
2021-10-30 11:28:03 +0200

    Add project files.

---
+++ KEZ-OS-TESTS/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KEZ-OS-TESTS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KEZ-OS-TESTS")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3e04fd9-5b13-4aa3-a970-452c2e44f211")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ deploy/KEZ-OS/script.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * R e a d m e
+ * -----------
+ * 
+ * In this file you can include any instructions or other comments you want to have injected onto the 
+ * top of your final script. You can safely delete this file if you do not want any such comments.
+ */
+
+
+public Program()
+{
+
+}
+
+public void Save()
+{
+
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+    Class1 class1 = new Class1();
+    Echo(class1.qweasd(true));
+}
+
+public class Class1
+{
+    public string qweasd(bool qwe)
+    {
+        return qwe ? "qwe" : "asd";
+    }
+}
